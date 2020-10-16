b76e42a004dc395670b63bca172d312f03704ebe
DESKTOP-64F13KA\user7 <aryayudhapratama2003@gmail.com>
2020-10-16 12:33:14 +0900

    no message

---
+++ biodataAwal.java
@@ -0,0 +1,23 @@
+package com.Biodata2;
+
+public class biodataAwal {
+
+    public void Nama(){
+        System.out.println("Nama : ARYA YUDHA PRATAMA");
+    }
+    public void Umur(){
+        System.out.println("Umur : 17 Th");
+    }
+    public void Ttl(){
+        System.out.println("Ttl : Cilacap, 05 September 2003");
+    }
+    public void Hobi(){
+        System.out.println("Hobi : Bermain Game dan Olahraga");
+    }
+    public void Sekolah(){
+        System.out.println("Sekolah : SMKN 1 PURWOSARI");
+    }
+    public void Keahlian(){
+        System.out.println("Keahlian : Rekayasa Perangkat Lunak (RPL)");
+    }
+}
---
+++ biodataUtama.java
@@ -0,0 +1,15 @@
+package com.Biodata2;
+
+public class biodataUtama {
+    public static void main(String[] args){
+
+        biodataAwal awal = new biodataAwal();
+
+        awal.Nama();
+        awal.Umur();
+        awal.Ttl();
+        awal.Hobi();
+        awal.Sekolah();
+        awal.Keahlian();
+    }
+}
